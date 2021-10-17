93cdebe50b24acb5088cb29fde5fbb914d9cbe5d
DavidAnacona <danaconac@ucentral.edu.co>
2021-10-17 15:20:02 -0500

    Cors

--- src/main/java/co/edu/unbosque/ciclo3back/api/UsuariosAPI.java
+++ src/main/java/co/edu/unbosque/ciclo3back/api/UsuariosAPI.java
@@ -1,11 +1,12 @@
 package co.edu.unbosque.ciclo3back.api;
 
-import co.edu.unbosque.ciclo3back.dao.mensaje;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import co.edu.unbosque.ciclo3back.dao.UsuariosDAO;
+import co.edu.unbosque.ciclo3back.dao.mensaje;
 import co.edu.unbosque.ciclo3back.model.Usuarios;
 
 
@@ -24,6 +27,7 @@ public class UsuariosAPI {
 	@Autowired 
 	private UsuariosDAO usuariosDAO;
 
+	@CrossOrigin(origins = "http://localhost:9001")
 	@GetMapping("/listar")
 	public List<Usuarios> listar() {
 		return usuariosDAO.findAll();
